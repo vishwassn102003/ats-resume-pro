199e5c83f47c9278f125f344e4fadddc27f6620e
Vishwas S N <snvishwas92@gmail.com>
2026-04-26 11:10:12 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://your-backend-url";
+const API = "https://ats-resume-pro-6x15.onrender.com"; // <-- YOUR BACKEND
 
 export const analyze = (data) => axios.post(`${API}/analyze`, data);
 export const improve = (data) => axios.post(`${API}/improve`, data);
