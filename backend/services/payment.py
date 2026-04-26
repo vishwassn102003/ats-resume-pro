2d3f502c0aa401043346f128803410074942e473
Vishwas S N <snvishwas92@gmail.com>
2026-04-26 08:27:13 +0530

    Update payment.py

--- backend/services/payment.py
+++ backend/services/payment.py
@@ -1,16 +1,1 @@
-import razorpay, os
-from fastapi import APIRouter
 
-router = APIRouter()
-
-client = razorpay.Client(auth=(
-    os.getenv("RAZORPAY_KEY_ID"),
-    os.getenv("RAZORPAY_KEY_SECRET")
-))
-
-@router.post("/payment")
-def create():
-    return client.order.create({
-        "amount": 2100,
-        "currency": "INR"
-    })
